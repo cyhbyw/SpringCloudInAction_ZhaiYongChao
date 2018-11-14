fff68849cbfba85c16ee22fef9514cea9014a303
cyhbyw <swust2009cyh@qq.com>
2018-11-14 08:24:03 +0800

    hystrix dashboard

---
+++ Chapter05_Hystrix/hystrix-dashboard/src/main/java/com/cyh/HystrixDashboardApplication.java
@@ -0,0 +1,18 @@
+package com.cyh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+/**
+ * @author: CYH
+ * @date: 2018/11/14 0014 8:09
+ */
+@EnableHystrixDashboard
+@SpringBootApplication
+public class HystrixDashboardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HystrixDashboardApplication.class, args);
+    }
+}
