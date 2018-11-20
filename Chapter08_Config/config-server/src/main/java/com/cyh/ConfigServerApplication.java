ad89375269815d9d21cdce075e845d737917dc31
cyhbyw <swust2009cyh@qq.com>
2018-11-20 08:19:21 +0800

    提交以在Git上产生对应目录及文件，方便后续配置

---
+++ Chapter08_Config/config-server/src/main/java/com/cyh/ConfigServerApplication.java
@@ -0,0 +1,19 @@
+package com.cyh;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @author: CYH
+ * @date: 2018/11/20 0020 8:09
+ */
+@EnableConfigServer
+@SpringBootApplication
+public class ConfigServerApplication {
+
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(ConfigServerApplication.class).web(true).run(args);
+    }
+
+}
